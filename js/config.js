ce511fc467029e817df01efa1d75424e464a69eb
EMH <32689497+mehdihanani@users.noreply.github.com>
2025-04-07 10:43:37 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ca-central-1_ksK2PQHGm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '43f0b4l5p45j9bfg3u71nr4ebe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ca-central-1_FWPkhnUPe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '48e2tr0amcqum0069b48acorv2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
